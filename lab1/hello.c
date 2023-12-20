a7e502030240d108799b742291c29ebee7316413
Edison Zhang <zhang.12789@osu.edu>
2023-12-20 03:37:21 +0000

    setup for lab1

---
+++ lab1/hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/* Author: Edison Zhang */
+int main(void)
+{
+  char string1[] = "The quick fox jumped over the brown dog.";
+  int i;
+  int max = 1;
+  for (i = 0; i < max; i++) {
+    printf("Hello, World!\n");
+  }
+  printf("The sample sentence is \"%s\".\n", string1);
+  return (0);
+}
